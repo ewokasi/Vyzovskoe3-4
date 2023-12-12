3d13492ec2d99237460826f77c1ef6851d6c9a33
ewokasi <45501886+ewokasi@users.noreply.github.com>
2023-12-12 16:10:13 +0300

    12.12

--- 5 сем/Прикладные модели оптимизации/Задание 3/main.py
+++ 5 сем/Прикладные модели оптимизации/Задание 3/main.py
@@ -1,24 +1,23 @@
-from scipy.optimize import linprog
+from pulp import *
+import time
 
-obj = [-1, -3]
-#      ─┬  ─┬
-#       │   └┤ Коэффициент для y
-#       └────┤ Коэффициент для x
+x1 = pulp.LpVariable("x1", lowBound=0, cat=LpInteger)
+x2 = pulp.LpVariable("x2", lowBound=0, cat=LpInteger)
+x3 = pulp.LpVariable("x3", lowBound=0, cat=LpInteger)
+x4 = pulp.LpVariable("x4", lowBound=0, cat=LpInteger) #определяем переменные кол-ва товара
 
-lhs_ineq = [[ 2,  1], 
-            [1,  -1],  
-            [ 1, -1]]  
+problem = pulp.LpProblem('0', LpMaximize) #условие на максимум
 
-rhs_ineq = [2,  
-            0,  
-            1]
-           
+problem += 3100*x1+4200*x2+7000*x3+2000*x4, "Функция цели" #переносим матрицу
+problem += 10*x1+20*x2+8*x3+15*x4<=200, "1"
+problem += 20*x1+10*x2+10*x3+30*x4<=500, "2"
+problem += 2*x1+3*x2+5*x3+1*x4<=30, "3"
+problem.solve() #запускаем расчет
+
+print ("Результат:")
+for variable in problem.variables():
+    print (variable.name, "=", variable.varValue)
+print ("Прибыль:")
+print (value(problem.objective))
 
 
-bnd = [(0, float("inf")),  # Границы x
-       (0, float("inf"))]  # Границы y
-
-opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd)
-
-print(opt)
-print("оптимальное решение: X:",max(opt.x))
